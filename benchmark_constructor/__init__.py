bb1c415baace22e4c7b46dc2737bc668dce8a37c
xingjiepan <xingjiepan@gmail.com>
2016-07-28 15:42:37 -0700

    File rearrangement

--- benchmark_constructor/__init__.py
+++ benchmark_constructor/__init__.py
@@ -1,9 +1,9 @@
-from . import DataController
-
 from . import structure_collectors
 
 from . import filters
 
 from . import file_normalizers
+
+from . import job_distributors
 
 from . import flow_control
