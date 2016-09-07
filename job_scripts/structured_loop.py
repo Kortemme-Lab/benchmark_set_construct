504f9bde468c214cc46cdfc7481fddba51939b30
xingjiepan <xingjiepan@gmail.com>
2016-09-07 13:49:51 -0700

    Update the structured_loop.py script

--- job_scripts/structured_loop.py
+++ job_scripts/structured_loop.py
@@ -24,11 +24,14 @@ if __name__ == '__main__':
               BC.filters.StructuredLoopFilter(12, 5, model=0),
               BC.filters.LoopCrystalContactFilter(4, model=0, chain_list=['A'], pymol_bin='pymol'),
               BC.filters.TerminalLoopFilter(5),
-							] 
+							BC.filters.DiscardLoopFilter(1),
+              ] 
   
   # Register normalizers
   
   normalizers = [ BC.file_normalizers.RosettaLoopNormalizer(),
+                  BC.file_normalizers.LoopFileNormalizer(),
+                  BC.file_normalizers.LoopTrimNormalizer(10),
                   # If you have the clean_pdb.py script from Rosetta tools. Uncomment the following line and change path to where the script is.
                   #BC.file_normalizers.RosettaCleanPDBNormalizer('/kortemmelab/home/xingjiepan/Softwares/Rosetta/tools/protein_tools/scripts/clean_pdb.py'),
                   ] 
