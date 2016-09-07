efc8dbb6702663f8364ae851a81886ca2d41402f
xingjiepan <xingjiepan@gmail.com>
2016-09-07 11:59:52 -0700

    Add the get_residues_nearby() function

--- benchmark_constructor/utilities/metric.py
+++ benchmark_constructor/utilities/metric.py
@@ -1,3 +1,5 @@
+import Bio.PDB as PDB
+
 from .Chemistry import is_heavy_atom
 
 def residue_pair_distance(res1, res2, heavy_only=True):
@@ -35,3 +37,18 @@ def residue_group_distance(res_list1, res_list2, heavy_only=True):
         distance = d
 
   return distance
+
+
+def get_residues_nearby(res_list, structure, cutoff):
+  '''Get a list of residues whose CA-CA distance from the given residue list is within
+     the cutoff.
+  '''
+  center_atoms = [atom for res in res_list for atom in res if atom.name == 'CA']
+  atom_list = [atom for atom in structure.get_atoms() 
+               if (atom.name == 'CA' and not atom in center_atoms)]
+
+  ns = PDB.NeighborSearch(atom_list)
+  nearby_residues = {atom.get_parent() for center_atom in center_atoms
+                       for atom in ns.search(center_atom.coord, cutoff, 'A')} 
+
+  return list(nearby_residues)
