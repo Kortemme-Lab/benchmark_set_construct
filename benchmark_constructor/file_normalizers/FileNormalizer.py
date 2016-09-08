8460aa0102465548c031e2f2d225aa2b7e7307bc
xingjiepan <xingjiepan@gmail.com>
2016-09-08 15:42:21 -0700

    Add the UpdatePDBNormalizer

--- benchmark_constructor/file_normalizers/FileNormalizer.py
+++ benchmark_constructor/file_normalizers/FileNormalizer.py
@@ -1,4 +1,16 @@
+import shutil
+
+
 class FileNormalizer():
   '''Base class for file normalizers'''
   def __init__(self):
     pass
+
+
+class UpdatePDBNormalizer(FileNormalizer):
+  '''Base class for file normalizers that update the PDB file'''
+  def __init__(self):
+    pass
+
+  def update_pdb(self, new_pdb_path, pdb_path):
+    shutil.move(new_pdb_path, pdb_path)
