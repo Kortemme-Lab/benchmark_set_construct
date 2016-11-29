2fde88bd64a0754eb9294a26ace2614b6bdb88e1
xingjiepan <xingjiepan@gmail.com>
2016-11-29 13:32:50 -0800

    Add a script to get the top8000 data set.

---
+++ inputs/get_top8000.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://kinemage.biochem.duke.edu/php/downlodekins.php?filename=/downloads/datasets/top8000_chains_70.tgz
+
+tar -xzf 'downlodekins.php?filename=%2Fdownloads%2Fdatasets%2Ftop8000_chains_70.tgz'
+rm 'downlodekins.php?filename=%2Fdownloads%2Fdatasets%2Ftop8000_chains_70.tgz'
